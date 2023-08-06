1e1702cbdeed2beda7fcf992aeac9da6480f9f79
theo silva <theodorotheodore@gmail.com>
2023-08-06 20:57:34 -0300

    wip(use-cases/create-voting, repositories/write-through): implementing use case + persistence solution

--- app/src/repositories/write-through.ts
+++ app/src/repositories/write-through.ts
@@ -1,3 +1,13 @@
-export class WriteThrough {
+import { PrismaClient, Voting } from '@prisma/client';
+import { RedisClientType } from '@redis/client';
 
+export class WriteThrough {
+  constructor(
+    private readonly PrismaManager: PrismaClient,
+    private readonly RedisManager: RedisClientType,
+  ) {}
+
+  public async writeVotingSession(voting: Voting) {
+    const 
+  }
 }
