3a7f6401b2ae3db8e88bc626fbf275e0217369af
theo silva <theodorotheodore@gmail.com>
2023-08-08 04:12:47 -0300

    feat(use-cases/create-voting, repositories/write-through): implement solution

--- app/src/repositories/write-through.ts
+++ app/src/repositories/write-through.ts
@@ -1,6 +1,7 @@
+import { v1 as uuid } from 'uuid';
 import { PrismaClient, Voting } from '@prisma/client';
 import { RedisClientType } from '@redis/client';
-import { v5 as uuid } from 'uuid';
+import { logger } from '@utils/logger';
 
 export class WriteThrough {
   constructor(
@@ -9,21 +10,24 @@ export class WriteThrough {
   ) {}
 
   public async writeVotingSession(name: string, options: Array<string>): Promise<Voting> {
-    const votingUUID = uuid(name, 'voting');
+    const votingUUID = uuid();
     const votingState = 'OPEN';
 
+    logger.info(`Caching voting options to voting "${name}"...`);
     const votingOptionsKey = `${votingUUID}:options`;
     const votingOptionsValue = options.join(',');
     await this.RedisManager.set(votingOptionsKey, votingOptionsValue);
 
+    logger.info(`Caching voting state to voting "${name}"...`);
     const votingStateKey = `${votingUUID}:state`;
     await this.RedisManager.set(votingStateKey, votingState);
 
+    logger.info(`Storing voting "${name}" in database...`);
     const voting = await this.PrismaManager.voting.create({
       data: {
         uuid: votingUUID, name, availableOptions: options, state: votingState,
       },
-    }).then((value) => value);
+    });
 
     return voting;
   }
