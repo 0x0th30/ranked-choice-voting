afc0ca6b1b3e36b9c3507df1408560c39a54a372
theo silva <theodorotheodore@gmail.com>
2023-08-07 22:49:22 -0300

    fix: update database schema

--- app/prisma/migrations/20230808014601_initial/migration.sql
+++ app/prisma/migrations/20230808014601_initial/migration.sql
@@ -3,15 +3,15 @@ CREATE TYPE "VotingState" AS ENUM ('OPEN', 'CLOSED');
 
 -- CreateTable
 CREATE TABLE "Voting" (
-    "sessionId" TEXT NOT NULL,
-    "sessionName" TEXT NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "availableOptions" TEXT[],
     "state" "VotingState" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Voting_pkey" PRIMARY KEY ("sessionId")
+    CONSTRAINT "Voting_pkey" PRIMARY KEY ("uuid")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Voting_sessionId_key" ON "Voting"("sessionId");
+CREATE UNIQUE INDEX "Voting_uuid_key" ON "Voting"("uuid");
