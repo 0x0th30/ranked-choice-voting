72310169a1ca130ce892800f4aff67a04de7f277
theo silva <theodorotheodore@gmail.com>
2023-10-13 06:12:37 -0300

    refactor(repositories/write-through): update variable names

--- app/src/repositories/write-through.ts
+++ app/src/repositories/write-through.ts
@@ -10,38 +10,42 @@ export class WriteThrough {
 
   public async writeVoting(
     userUUID: string,
-    uuid: string,
+    votingUUID: string,
     name: string,
     options: Array<string>,
     state: VotingState,
   ): Promise<Voting> {
     logger.info(`Caching voting options to voting "${name}"...`);
-    const votingOptionsKey = `${uuid}:options`;
+    const votingOptionsKey = `${votingUUID}:options`;
     const votingOptionsValue = options.join(',');
     await this.RedisManager.set(votingOptionsKey, votingOptionsValue);
 
     logger.info(`Caching voting state to voting "${name}"...`);
-    const votingStateKey = `${uuid}:state`;
+    const votingStateKey = `${votingUUID}:state`;
     await this.RedisManager.set(votingStateKey, state);
 
     logger.info(`Storing voting "${name}" in database...`);
     const voting = await this.PrismaManager.voting.create({
       data: {
-        uuid, name, available_options: options, state, author_uuid: userUUID,
+        uuid: userUUID,
+        author_uuid: votingUUID,
+        name,
+        available_options: options,
+        state,
       },
     });
 
     return voting;
   }
 
-  public async updateVotingState(uuid: string, state: VotingState): Promise<any> {
-    logger.info(`Caching voting state to voting with uuid "${uuid}"...`);
-    const votingStateKey = `${uuid}:state`;
+  public async updateVotingState(votingUUID: string, state: VotingState): Promise<any> {
+    logger.info(`Caching voting state to voting with votingUUID "${votingUUID}"...`);
+    const votingStateKey = `${votingUUID}:state`;
     await this.RedisManager.set(votingStateKey, state);
 
-    logger.info(`Updating voting with uuid "${uuid}" state in database...`);
+    logger.info(`Updating voting with votingUUID "${votingUUID}" state in database...`);
     const voting = await this.PrismaManager.voting.update({
-      where: { uuid },
+      where: { uuid: votingUUID },
       data: { state },
     });
 
