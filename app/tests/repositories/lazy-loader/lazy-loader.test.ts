e9f108b61dc18de98ec23abd2b25b8891a364a25
theo silva <theodorotheodore@gmail.com>
2023-10-16 04:53:19 -0300

    test(repositories/lazy-loader): write SUT base/mock sketch

--- app/tests/repositories/lazy-loader/lazy-loader.test.ts
+++ app/tests/repositories/lazy-loader/lazy-loader.test.ts
@@ -10,8 +10,33 @@ const LazyLoaderSUT = new LazyLoader(
 );
 
 describe('LazyLoader class', () => {
-  describe('(public) isUniqueVotePerUser method', () => {});
-  describe('(public) readVotingState method', () => {});
-  describe('(public) readVotingOptions method', () => {});
+  describe('(public) isUniqueVotePerUser method', () => {
+    it.todo('should call "this.RedisManager.get" method to search vote');
+    it.todo('should call "this.RedisManager.get" using correct key format');
+    it.todo('should return false if not found data in Redis');
+    it.todo('should return true if found data in Redis');
+    it.todo('should call "this.PrismaManager.findFirst" method to search vote');
+    it.todo('should call "this.PrismaManager.findFirst" using voting and user UUID');
+    it.todo('should return false if not found data in Prisma');
+    it.todo('should return true if found data in Prisma');
+  });
+  describe('(public) readVotingState method', () => {
+    it.todo('should call "this.RedisManager.get" method to search voting state');
+    it.todo('should call "this.RedisManager.get" using correct key format');
+    it.todo('should return voting state if found something in Redis');
+    it.todo('should call "this.PrismaManager.findFirst" method to search vote');
+    it.todo('should call "this.PrismaManager.findFirst" using voting and user UUID');
+    it.todo('should return voting options if found something in Prisma');
+    it.todo('should throw "NotFoundVoting" if cannot found it');
+  });
+  describe('(public) readVotingOptions method', () => {
+    it.todo('should call "this.RedisManager.get" method to search voting options');
+    it.todo('should call "this.RedisManager.get" using correct key format');
+    it.todo('should return voting options if found something in Redis');
+    it.todo('should call "this.PrismaManager.findFirst" method to search vote');
+    it.todo('should call "this.PrismaManager.findFirst" using voting and user UUID');
+    it.todo('should return voting options if found something in Prisma');
+    it.todo('should throw "NotFoundVoting" if cannot found it');
+  });
 });
 
